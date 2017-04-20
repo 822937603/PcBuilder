d4a7f545ec29e3d1a9ac84bf83336d2b8ea762d5
Jeff Li <jeff.li.cf@gmail.com>
2017-04-19 23:21:36 -0400

    Added Menus and restart function
    bless

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
 
 	public float timer;
 	public Text timerLabel;
 	public Text GOLabel;
+	public Text restartLabel;
 	public MouseClick mouseClick;
 
 	private AudioSource[] _audioSources;
 	private AudioSource _tick;
+	private bool restart;
 
 
 	// Use this for initialization
 	void Start () {
 		GOLabel.enabled = false;
+		restartLabel.enabled = false;
 		//this._audioSources = gameObject.GetComponents<AudioSource>();
 		//this._tick = this._audioSources[0];
 	}
@@ -26,6 +30,12 @@ public class Timer : MonoBehaviour {
 	void Update () {
 		timer -= Time.deltaTime;
 		this.Timers();
+
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene ("scene-1");
+			}
+		}
 
 	}
 	/*void PlayTicking()
@@ -48,6 +58,8 @@ public class Timer : MonoBehaviour {
 			timerLabel.text = "0:00";
 			GOLabel.enabled = true;
 			mouseClick.enabled = false;
+			restartLabel.enabled = true;
+			restart = true;
 		}
 	}
 }
